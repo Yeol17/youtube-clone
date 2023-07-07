cecc43e0a2f38dd8aba656b2e1d18485d9b8d99c
Yeol17 <hsl0717@naver.com>
2023-07-07 20:59:14 +0900

    chore: react-router-dom 설치

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import Header from './components/header/Header'
 import Modal from './components/dialog/Modal'
 import { useSelector } from 'react-redux';
+import { NavMax, NavMin } from './components/navigation/Nav'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Header />
       {popUp.shortcutOpend && <Modal />}
+      <NavMin />
     </div>
 
   );
---
+++ src/components/navigation/Nav.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+
+import {
+  MdSubscriptions, MdOutlineVideoLibrary,
+  MdOutlineFlag, MdOutlineVideogameAsset,
+  MdMusicNote, MdOutlineLightbulb, MdHelpOutline,
+  MdLocalFireDepartment
+} from 'react-icons/md'
+import { GoHomeFill, GoPlusCircle } from 'react-icons/go'
+import { IoTrophyOutline } from 'react-icons/io'
+import { CiStreamOn } from 'react-icons/ci'
+import { SiYoutubegaming } from 'react-icons/si'
+import { AiOutlineShopping } from 'react-icons/ai'
+
+export function NavMax() {
+  return (
+    <nav>
+    </nav>
+  )
+}
+
+export function NavMin() {
+  return (
+    <nav>
+
+      <MdSubscriptions />
+    </nav>
+  )
+}
