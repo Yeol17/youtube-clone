99ce237139602674db6594617c043267155f313c
Yeol17 <hsl0717@naver.com>
2023-07-10 16:50:49 +0900

    Fix: 축소형 시 5개만 나오도록 코드 수정

--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -11,6 +11,7 @@ import {
 import { GoPlusCircle, GoHistory } from 'react-icons/go'
 import { IoTrophyOutline } from 'react-icons/io'
 import { VscHistory } from 'react-icons/vsc'
+import { HiOutlineFire, HiFire } from 'react-icons/hi'
 import { CiStreamOn } from 'react-icons/ci'
 import { RiHistoryFill } from 'react-icons/ri'
 import { SiYoutubegaming } from 'react-icons/si'
@@ -40,17 +41,19 @@ export function NavMin() {
     setPage(location.pathname);
   }, [location])
 
-  let lists = navigation.map(list => {
-    return (
-      <li className='min-itm'>
-        <Link to={list.url} className='menu-min'>
-          {page === list.url ? list.active : list.inactive}
-          <p className='text'>{list.title}</p>
+  let lists = [];
+
+  for(let i = 0; i < 5; i++) {
+    lists.push(
+      <li className='min-itm' key={navigation[i].id}>
+        <Link to={navigation[i].url} className='menu-min'>
+          {page === navigation[i].url ? navigation[i].active : navigation[i].inactive}
+          <p className='text'>{navigation[i].title}</p>
         </Link>
       </li>
     )
-  })
-  
+  }
+
   return (
     <nav>
       <ul>
@@ -66,4 +69,5 @@ const navigation = [
   { id: 102, title: '구독', url: '/feed/subscription', active: <MdSubscriptions />, inactive: <MdOutlineSubscriptions /> },
   { id: 200, title: '보관함', url: '/feed/library', active: <MdVideoLibrary />, inactive: <MdOutlineVideoLibrary /> },
   { id: 201, title: '시청 기록', url: '/feed/history', active: <VscHistory className='bold' />, inactive: <VscHistory /> },
+  { id: 301, title: '인기 급상승', url: '/feed/trending', active: <HiFire />, inactive: <HiOutlineFire /> },
 ];
