57c10b0a493f58e4f3abfca46eac3686f4c23444
Yeol17 <hsl0717@naver.com>
2023-09-02 21:48:31 +0900

    배포용 환경변수 추가

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import { store } from './store'
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom'
-
+console.log(process.env);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
             <App />
         </BrowserRouter>
     </Provider>
